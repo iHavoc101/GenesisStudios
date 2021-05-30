7ad056140f962de740bc806fd60b8d0db5ff348c
iHavoc101 <69701784+iHavoc101@users.noreply.github.com>
2021-05-30 15:15:25 -0400

    Update and rename src/Build.lua to Build.lua

--- Build.lua
+++ Build.lua
@@ -1,7 +1,7 @@
 -- // Functions \\ ==
 local function RetrieveSource(Link, Root)
     Link = tostring(Link)
-    if string.match(Link, "^https://raw.githubusercontent.com/") then
+    if string.match(Link, "^https://") then
         return game:HttpGet(Link, true)
     elseif Root then
         return game:HttpGet("https://raw.githubusercontent.com/iHavoc101/GenesisStudios/main/" .. Link)
@@ -12,21 +12,30 @@ end
 
 -- // Variables \\ --
 local LoadInterface = loadstring(isfile("User Interface/LoadInterface.lua") and readfile("User Interface/LoadInterface.lua") or RetrieveSource("User%20Interface/LoadInterface.lua"))()
+local CurrentBuild = RetrieveSource("CurrentBuild.json")
 
 -- // Actions \\ --
 -- [ Folders ] --
+LoadInterface.SetTitle(string.format("Genesis Studios <b>%s</b>", game:GetService("HttpService"):JSONDecode(CurrentBuild).Version))
 LoadInterface.SetStatus("Building Folders...")
 LoadInterface.SetProgress(0)
+
+-- Main Folders --
 pcall(makefolder, "Genesis Studios/Core")
+pcall(makefolder, "Genesis Studios/Assets")
 pcall(makefolder, "Genesis Studios/User Interface")
 pcall(makefolder, "Genesis Studios/Modules")
 pcall(makefolder, "Genesis Studios/Libraries")
 pcall(makefolder, "Genesis Studios/Scripts")
 
--- [ Lazy Loader ] --
-LoadInterface.SetStatus("Building LazyLoader.lua...")
-LoadInterface.SetProgress(5)
+-- Sub Folders --
+pcall(makefolder, "Genesis Studios/Assets/Logos")
+
+-- [ Build ] --
+LoadInterface.SetStatus("Loading Builder...")
+LoadInterface.SetProgress(10)
 pcall(writefile, "Genesis Studios/LazyLoader.lua", RetrieveSource("LazyLoad.lua"))
+pcall(writefile, "Genesis Studios/CurrentBuild.json", CurrentBuild)
 
 -- Core --
 LoadInterface.SetStatus("Building Core...")
@@ -36,14 +45,19 @@ pcall(writefile, "Genesis Studios/Core/Maid.lua", RetrieveSource("Core/Maid.lua"
 pcall(writefile, "Genesis Studios/Core/Input.lua", RetrieveSource("Core/Input.lua"))
 pcall(writefile, "Genesis Studios/Core/RBXScriptSignal.lua", RetrieveSource("Core/RBXScriptSignal.lua"))
 
+-- Assets --
+LoadInterface.SetStatus("Creating Assets...")
+LoadInterface.SetProgress(40)
+--pcall(writefile, "Genesis Studios/Assets/Logos/Main.png", "")
+
 -- User Interface --
 LoadInterface.SetStatus("Building Interface...")
-LoadInterface.SetProgress(35)
+LoadInterface.SetProgress(50)
 pcall(writefile, "Genesis Studios/User Interface/LoadInterface.lua", RetrieveSource("User%20Interface/LoadInterface.lua"))
 
 -- Modules --
 LoadInterface.SetStatus("Building Modules...")
-LoadInterface.SetProgress(50)
+LoadInterface.SetProgress(60)
 
 -- Libraries --
 LoadInterface.SetStatus("Building Libraries...")
@@ -57,9 +71,9 @@ LoadInterface.SetProgress(90)
 pcall(writefile, "Genesis Studios/Scripts/Genesis Hub.lua", "")
 
 -- [ Completion ] --
-wait(1)
-LoadInterface.SetStatus("Build Complete!")
+wait(0.2)
 LoadInterface.SetProgress(100)
-
+wait(0.25)
+LoadInterface.SetStatus("Build Complete!")
 wait(2.5)
 LoadInterface:Destroy()
